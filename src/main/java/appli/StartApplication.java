2520c43e75b0204d799eb24183bf60fddc56a6bf
Reyan BABOU <BABOU_Re@lprs.local>
2025-03-06 17:24:30 +0100

    [feat] deuxieme commit

--- src/main/java/appli/StartApplication.java
+++ src/main/java/appli/StartApplication.java
@@ -1,4 +1,4 @@
-package appli.todolistfx;
+package appli;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class StartApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("LoginView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("connexion");
         stage.setScene(scene);
         stage.show();
     }
